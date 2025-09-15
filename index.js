bbfe296d2761d5874fae775a0eebdfc9fe0250c6
koolgokul22 <rsgokulkumar@gmail.com>
2025-09-15 18:44:11 +0530

    ep1

---
+++ index.js
@@ -0,0 +1,29 @@
+const heading = React.createElement("div", { id: "" }, [
+  React.createElement("div", {}, [
+    React.createElement("p", {}, "Children"),
+    React.createElement("p", {}, "Children"),
+  ]),
+  React.createElement("h1", {}, "Sibling"),
+  React.createElement("div", {}, [
+    React.createElement("p", {}, "Children"),
+    React.createElement("p", {}, "Children"),
+  ]),
+  ,
+]);
+const root = ReactDOM.createRoot(document.getElementById("root"));
+
+root.render(heading);
+
+/**
+ * div
+ *      div
+ *          child1
+ *          child2
+ *      /div
+ *                  h1 /h1
+ *      div
+ *          child3
+ *          child4
+ *      /div
+ * /div
+ */
